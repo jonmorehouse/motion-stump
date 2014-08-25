6de534c396d5dc6004227ba40443312b5b433dc2
Jon Morehouse <morehousej09@gmail.com>
2014-08-24 18:59:48 -0700

    Add in ability to pass options to reset, this allows for selectively keeping methods, for cases where you can't remove them... ie: native objects

--- lib/stump/metareset.rb
+++ lib/stump/metareset.rb
@@ -8,22 +8,22 @@ class Object
     }
   end
 
-  def reset(method_name)
-    metaclass.restore_original_method(method_name)
+  def reset(method_name, opts = {})
+    metaclass.restore_original_method(method_name, opts)
   end
 
   protected
 
-  def remember_original_method(method_name)
+  def remember_original_method(method_name, opts = {})
     alias_method "__original_#{method_name}".to_sym, method_name if method_defined?(method_name)
     self
   end
 
-  def restore_original_method(method_name)
+  def restore_original_method(method_name, opts = {})
     original_method_name = "__original_#{method_name}".to_sym
     if method_defined?(original_method_name)
       alias_method method_name, original_method_name
-      remove_method original_method_name
+      remove_method(original_method_name) unless opts.delete(:keep_method)
     end
     self
   end
